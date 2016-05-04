8b4faea37205df0a858ab0d8c5c5a2070cd9ae2f
Shiva Persad <shivapersad@gmail.com>
2016-05-04 11:21:58 +0100

    CMM007 - Assessed Lab

--- add.php
+++ add.php
@@ -25,35 +25,66 @@
     </header>
 
     <main class="grid-80">
-        <form method="post">
-            <table>
-                <tr>
-                    <td>Entry Title:</td>
-                    <td><input type="text" name="title" required></td>
-                </tr>
-                <tr>
-                    <td>Entry Summary:</td>
-                    <td><textarea rows="5" cols="40" name="summary" required></textarea></td>
-                </tr>
-                <tr>
-                    <td>Category:</td>
-                    <td>
-                        <select name="category" id="category" required>
-                            <option value="Work">Work</option>
-                            <option value="University">University</option>
-                            <option value="Family">Family</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Submitted By:</td>
-                    <td><input type="text" name="submitted"></td>
-                </tr>
-                <tr colspan="2">
-                    <td><input type="submit" value="Submit"></td>
-                </tr>
-            </table>
-        </form>
+        <?
+            if ($_SERVER['REQUEST_METHOD'] === 'GET')
+            {
+        ?>
+                <form method="post">
+                    <table>
+                        <tr>
+                            <td>Entry Title:</td>
+                            <td><input type="text" name="title" required></td>
+                        </tr>
+                        <tr>
+                            <td>Entry Summary:</td>
+                            <td><textarea rows="5" cols="40" name="summary" required></textarea></td>
+                        </tr>
+                        <tr>
+                            <td>Category:</td>
+                            <td>
+                                <select name="category" id="category" required>
+                                    <option value="Work">Work</option>
+                                    <option value="University">University</option>
+                                    <option value="Family">Family</option>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Submitted By:</td>
+                            <td><input type="text" name="submitted"></td>
+                        </tr>
+                        <tr colspan="2">
+                            <td><input type="submit" value="Submit"></td>
+                        </tr>
+                    </table>
+                </form>
+                <?
+            }
+            elseif ($_SERVER['REQUEST_METHOD'] === 'POST')
+                {
+                    include_once('connection.php');
+
+                    $title = $_POST['title'];
+                    $summary = $_POST['summary'];
+                    $category = $_POST['category'];
+                    $submitted = $_POST['submitted'];
+
+                    if ($submitted == NULL)
+                    {
+                        $submitted = "Anonynmous";
+                    }
+
+                    $sql = "INSERT INTO blogView VALUES (NULL,'$title','$summary','$category','$submitted')";
+
+                    mysqli_query($conn,$sql);
+                    mysqli_close($conn);
+                    header('Location: blog.php');
+                }
+                else
+                {
+                    header('Location: index.php');
+                }
+                ?>
     </main>
 
     <footer class="grid-80">
